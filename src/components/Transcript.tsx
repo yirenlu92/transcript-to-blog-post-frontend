4350821683f84fa4c15d4302022310cab236b70f
Yiren Lu <yirenlu13@gmail.com>
2023-03-09 13:26:57 -0500

    Change the prompt

--- src/components/Transcript.tsx
+++ src/components/Transcript.tsx
@@ -3,7 +3,37 @@ import axios from 'axios';
 import TextEditor from './TextEditor'
 import './Transcript.css'
 
+
 interface Props {}
+
+// store popular prompts
+// const prompts = [
+//   "
+//         Please translate the following interview transcript into complete, grammatical, idiomatic American English sentences in the style of a professional translator. Do not omit details or change the meaning of the original text.
+//     ",
+
+//     """
+//     Please turn the following interview transcript into a 600-word customer success story that begins: "We spoke with(name and title at company) about their vision for how Azure OpenAI Service will transform XXX. This is that conversation as summarized by Azure OpenAI Service."
+
+//     Stories should look and feel like they could live on the new Azure AI hub and support the messaging and value statements contained within.
+
+//     The interview transcript is interviewing an educational provider in Taiwan that used Azure OpenAI Service for chatbot and speech assessment capabilities.
+
+//     The interview transcript is in Chinese and is too long, so we will split it up and give it to you in chunks. Please output the corresponding section of the blog post for each input transcript portion we give you. The section should be given a subheading that corresponds to the question that Yiren Lu asked.
+//     """
+
+//     """
+//     I would like you to take an interview transcript where Yiren Lu is interviewing {} on her experience transitioning from software engineering to product management, and rewrite it in complete, grammatical, idiomatic American English sentences in the style of a blog post.
+
+//     Some addition context about {}'s professional background:
+
+//     {}
+
+//     # The blog post should be written in the first person, from {}'s perspective, and retain as much of the original sentences and details from the transcript as possible. It should not say things like "Yiren Lu asked me...". You might have re-organize the text so that the chronology makes sense.
+    
+//     # Since the transcript is too long to fix in your context window, we will split it up and give it to you in chunks. Please output the corresponding section of the blog post for each input transcript portion we give you. The section should be given a subheading that corresponds to the question that Yiren Lu asked.
+//     #"
+// ]
 
 const Transcript: React.FC<Props> = () => {
   const [transcript, setTranscript] = useState<string>('');
@@ -11,7 +41,7 @@ const Transcript: React.FC<Props> = () => {
   const [intervieweeName, setIntervieweeName] = useState<string>('');
   const [intervieweeBackground, setIntervieweeBackground] = useState<string>('');
   const [blogPost, setBlogPost] = useState<string>('');
-
+  const [prompt, setPrompt] = useState<string>('');
 
   const handleFirefliesIdSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 
@@ -69,6 +99,7 @@ const Transcript: React.FC<Props> = () => {
 
     if (transcript) {
       formData.append('transcript', transcript);
+      formData.append('prompt', prompt);
       formData.append('interviewee_name', intervieweeName);
       formData.append('interviewee_background', intervieweeBackground);
     }
@@ -106,6 +137,13 @@ const Transcript: React.FC<Props> = () => {
           {transcript && <div>  
           <form onSubmit={handleSubmit}>
             <div>
+              <div>
+                <div>
+                  <label>Prompt: </label>
+                </div>
+                <TextEditor text={prompt} setText={setPrompt}/>
+                {/* <textarea className="prompt-box" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Prompt" /> */}
+              </div>
               <div>
                 <label>Interviewee Name: </label>
               </div>
