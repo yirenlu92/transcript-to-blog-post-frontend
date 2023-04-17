f1f067c615bdb338d3263aec0ae704fadabd5208
Yiren Lu <yirenlu13@gmail.com>
2023-04-17 16:05:12 -0400

    Make all the font bigger

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontSize: {
+      base: "1.25rem", // Change the base font size to 20px (1.25rem)
+      sm: "1rem",
+      md: "1.5rem",
+      lg: "1.875rem",
+      xl: "2.25rem",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
